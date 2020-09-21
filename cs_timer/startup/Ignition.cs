7bd3e98a73130593eb0062f83a469ab1a1ac0683
Christ <43361220+Nightstars@users.noreply.github.com>
2020-09-21 12:20:39 +0800

    Update Ignition.cs
    mirror bug fix

--- cs_timer/startup/Ignition.cs
+++ cs_timer/startup/Ignition.cs
@@ -1,4 +1,22 @@
-﻿using cs_timer.utils;
+/* -----------------------------------------------------------------------------
+ 1.    INTRODUCTION
+This project is a guide program on how to use the console to perform timing tasks
+on the .net core platform, so that the development tasks that require this
+business need do not need to focus on the realization of timing tasks, but pay 
+more attention to actual business functions. This project supports configuration 
+Files, allowing dynamic adjustment of the timing point after the version is 
+released, temporarily only supports daily timing, which can be accurate to the 
+second level.
+2.    CONTACT INFORMATION
+chrsitzhangowner@gmail.com
+----------------------------------------------------------------------------- */
+
+/************************ cs_timer .net core support *************************
+   Author(s):   christ chang
+   Description: cs_timer .net core support
+   github: https://github.com/Nightstars/cs_timer
+*******************************************************************************/
+using cs_timer.utils;
 using System;
 using System.Collections.Generic;
 using System.Text;
