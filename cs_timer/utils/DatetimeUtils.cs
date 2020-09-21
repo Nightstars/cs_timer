a20ad067fc6cee86bd371288359a181a909e29c1
Christ <43361220+Nightstars@users.noreply.github.com>
2020-09-21 12:30:30 +0800

    Update DatetimeUtils.cs

--- cs_timer/utils/DatetimeUtils.cs
+++ cs_timer/utils/DatetimeUtils.cs
@@ -1,8 +1,8 @@
 /* -----------------------------------------------------------------------------
  1.    INTRODUCTION
 This project is a guide program on how to use the console to perform timing tasks
-on the .net core platform, so that the development tasks that require this
-business need do not need to focus on the realization of timing tasks, but pay 
+on the .net core platform, so that the developer who require this
+business do not need to focus on the realization of timing tasks, but pay 
 more attention to actual business functions. This project supports configuration 
 Files, allowing dynamic adjustment of the timing point after the version is 
 released, temporarily only supports daily timing, which can be accurate to the 
