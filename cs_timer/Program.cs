a9e8c979fee2cf004d359525a4ef4d1c3d9f576e
Christ <43361220+Nightstars@users.noreply.github.com>
2020-09-21 12:23:44 +0800

    Update Program.cs
    mirror bug fix

--- cs_timer/Program.cs
+++ cs_timer/Program.cs
@@ -1,4 +1,22 @@
-﻿using cs_timer.startup;
+/* -----------------------------------------------------------------------------
+ 1.    INTRODUCTION
+This project is a guide program on how to use the console to perform timing tasks
+on the .net core platform, so that the development tasks that require this
+business need do not need to focus on the realization of timing tasks, but pay 
+more attention to actual business functions. This project supports configuration 
+Files, allowing dynamic adjustment of the timing point after the version is 
+released, temporarily only supports daily timing, which can be accurate to the 
+second level.
+2.    CONTACT INFORMATION
+chrsitzhangowner@gmail.com
+----------------------------------------------------------------------------- */
+
+/************************ cs_timer .net core support *************************
+   Author(s):   christ chang
+   Description: cs_timer .net core support
+   github: https://github.com/Nightstars/cs_timer
+*******************************************************************************/
+using cs_timer.startup;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
@@ -9,6 +27,11 @@ namespace cs_timer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("/********************** cs_timer .net core support ***********************");
+                Console.WriteLine("   Author(s):   christ chang");
+                Console.WriteLine("   Description: cs_timer .net core support");
+                Console.WriteLine("   github: https://github.com/Nightstars/cs_timer");
+                Console.WriteLine("************************************************************************/");
             var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
             string time = configuration.GetSection("SystemSettings")["taskPlanTime"];
             string dbtype = configuration.GetSection("DbSettings")["dbtype"];
